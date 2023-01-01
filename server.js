2b9a858e6d3238edb0ca5d677c6f0360a1f597cf
Vikrant <pvikrant248@gmail.com>
2023-01-01 18:02:36 +0530

    initial boilerplate code

---
+++ server.js
@@ -0,0 +1,21 @@
+
+const express = require("express");
+
+
+const config = require("./configs/db.config");
+
+
+const app = express();
+
+const db=require("./models");
+
+db.sequelize.sync({force:true})
+.then(() => {
+    console.log('DB synced');
+})
+
+
+
+app.listen(process.env.PORT,() => {
+    console.log('Application is running on port 8000')
+})
